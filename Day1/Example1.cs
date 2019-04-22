59e0450917e1a7e475d1b034b4ee39f8f60e9f85
youssef baddi <baddiyoussef85@gmail.com>
2019-04-22 12:49:50 +0100

    commit edit

--- Day1/Example1.cs
+++ Day1/Example1.cs
@@ -49,13 +49,35 @@ public class
         bool isTrue = true;
         Console.WriteLine(isTrue);
 
-
+        // int 32 bit
         int intValue = 10000;
         Console.WriteLine(intValue);
         Console.WriteLine(int.MaxValue);
         Console.WriteLine(int.MinValue);
 
         Console.WriteLine("#####################");
+
+        // long 64 bit
+        long k;
+        Console.WriteLine(long.MaxValue);
+        Console.WriteLine(long.MinValue);
+
+        // decimal 128 bits
+        // with 28 digit
+
+        decimal d = 3.14526399938784849M;
+        Console.WriteLine(d);
+
+        // double 64 avec vergule
+            
+        // float
+
+        // byte 8-bit
+        //sbyte
+        // char
+        // short 
+        // ulong
+        // ushort
 
         Console.Read();
     }
