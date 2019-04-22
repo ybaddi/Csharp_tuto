5e8bc352f52e80d9b321ef0301a2da1f3008f7ec
youssef baddi <baddiyoussef85@gmail.com>
2019-04-22 12:31:25 +0100

    commit edit

--- Day1/Example1.cs
+++ Day1/Example1.cs
@@ -9,19 +9,47 @@ public class
 	
 	Programm
 {
-	static void Main()
-        {
-    //console
-        Console.WriteLine("Hello World");
-        Console.Read();
-
-// one line comment
-
-        /*
-         * comment 
-         * plusieues ligne
-         */
-
-        }
+	static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+    
+                int x;
+                x = 11;
+                Console.WriteLine(x);
+               int x1 = 10;
+                Console.WriteLine(x1);
+    
+                int ageUtilisateur = 30;
+                int ageutilisateur = 20;
+                Console.WriteLine(ageUtilisateur);
+                Console.WriteLine(ageutilisateur);
+    
+                // nameProgramm 123 hello word
+                int i = 0;
+                for(i=0; i< args.Length; i++)
+                {
+                    Console.WriteLine(args[i]);
+                }
+    
+                string argsString = string.Join(",", args);
+                Console.WriteLine(argsString);
+    
+                string[] myArgs = Environment.GetCommandLineArgs();
+                for (i = 0; i < myArgs.Length; i++)
+                {
+                    Console.WriteLine(myArgs[i]);
+                }
+    
+                Console.WriteLine(Environment.UserName);
+    
+    
+                // Datatype
+    
+                // boolean
+                bool isTrue = true;
+                Console.WriteLine(isTrue);
+    
+                Console.Read();
+            }
 	
 }
